2d34d46e68b59332237469f4b44f9f15850f3980
yaua <phungngoctho@gmail.com>
2020-12-09 14:30:59 +0700

    Update and rename globaldefine.h to main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef GLOBALDEFINE_H
+#define GLOBALDEFINE_H
+
+#include <QObject>
+#include <QUrl>
+
+#define DEFAULT_TITLE   QObject::tr("Twitter")
+#define DEFAULT_URL     QString("https://twitter.com/")
+#define DEFAULT_WIDTH   (700)
+#define DEFAULT_HEIGHT  (800)
+#define DEFAULT_DESC    QString()
+#define DEFAULT_ICON    QString()
+#endif // GLOBALDEFINE_H
