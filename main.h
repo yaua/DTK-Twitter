b40cc27e43e997e5a51c77c1b37dbaa98ecbd3bb
yaua <phungngoctho@gmail.com>
2020-12-11 09:49:38 +0700

    Update main.h

--- main.h
+++ main.h
@@ -8,6 +8,4 @@
 #define DEFAULT_URL     QString("https://twitter.com/")
 #define DEFAULT_WIDTH   (700)
 #define DEFAULT_HEIGHT  (800)
-#define DEFAULT_DESC    QString()
-#define DEFAULT_ICON    QString()
 #endif // GLOBALDEFINE_H
